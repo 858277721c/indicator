c1a5c0c4ecc5ed8c87d974d911caedd905214890
zjun <565061763@qq.com>
2020-09-29 08:35:02 +0800

    IndicatorTrack

--- lib/src/main/java/com/sd/lib/indicator/track/IndicatorTrack.java
+++ lib/src/main/java/com/sd/lib/indicator/track/IndicatorTrack.java
@@ -17,7 +17,7 @@ public interface IndicatorTrack
     /**
      * 页面显示的百分比回调
      *
-     * @param position     第几页
+     * @param position     位置
      * @param showPercent  显示的百分比[0-1]
      * @param isEnter      true-当前页面处于进入状态，false-当前页面处于离开状态
      * @param isMoveLeft   true-内容向左移动，false-内容向右移动
@@ -26,9 +26,9 @@ public interface IndicatorTrack
     void onShowPercent(int position, float showPercent, boolean isEnter, boolean isMoveLeft, PositionData positionData);
 
     /**
-     * 某一页选中或者非选中回调
+     * 选中或者非选中回调
      *
-     * @param position     第几页
+     * @param position     位置
      * @param selected     true-选中，false-未选中
      * @param positionData 当前position对应Item的位置信息
      */
